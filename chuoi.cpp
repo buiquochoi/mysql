bfe9700ee727f5f025b96ccfca87bf253be976cf
buiquochoi <hoihobui1996@gmail.com>
2017-04-09 17:52:19 +0700

    Fist commit

---
+++ chuoi.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int main(){
+	char p1[10];
+	char p2[10];
+	cout<<"nhap vao s2:"; cin>>p2;
+	strcpy(p1,p2);
+	cout<<p1;	
+	return 0;
+}
